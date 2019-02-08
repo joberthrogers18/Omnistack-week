dc1285e556b48a49f246a0a4900670a74dcf3889
Joberth Rogers Tavares Costa <joberth.rogers18@gmail.com>
2019-02-08 10:55:29 -0300

    Adding the inputs in login page Save the value in state variable Save the username in localStorage in browser

--- frontend/src/pages/Login.js
+++ frontend/src/pages/Login.js
@@ -4,12 +4,37 @@ import './Login.css';
 import TwitterIcon from '../twitter.svg'   
 
 export default class Login extends Component {
+    //variable store inside the component react
+    state = {
+        username: '',
+    };
+
+    handleInputChange = event => {
+        this.setState({username: event.target.value}); //update info in state
+    }
+
+    handleSubmit = event => {
+        event.preventDefault(); //avoid the pattern behavious in form (post, get), redirect page
+
+        const {username} = this.state;
+
+        if(!username.length)return;
+
+        localStorage.setItem('@GoTwitter:username', username); //Save the storage browser and save info there
+
+        this.props.history.push('/timeline'); //redirect
+    }
+
+    // onChange is a event in input, witch execute every time when is update
   render() {
     return(
         <div className="login-wrapper">
             <img src={TwitterIcon} alt="Go twitter"/>
-            <form>
-                <input placeholder="Nome de usuário"/>
+            <form onSubmit = {this.handleSubmit}>
+                <input 
+                    value={this.state.username}
+                    onChange = {this.handleInputChange}
+                    placeholder="Nome de usuário"/>
                 <button type="submit" >Entrar</button>
             </form>
         </div>
