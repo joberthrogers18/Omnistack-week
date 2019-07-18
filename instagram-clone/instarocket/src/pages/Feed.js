67f60bf704844e3448f7b3879ca3eea370b4833f
Joberth Rogers <joberth.rogers18@gmail.com>
2019-07-17 21:38:50 -0400

    Made the NavBar main of app in instagram clone

--- instagram-clone/instarocket/src/pages/Feed.js
+++ instagram-clone/instarocket/src/pages/Feed.js
@@ -1,8 +1,20 @@
 import React, { Component } from "react";
 
-import { View } from "react-native";
+import { View, Image, TouchableOpacity } from "react-native";
+
+import camera from "../assets/camera.png";
 
 export default class Feed extends Component {
+  // quando estiver nesse componente ele vai adicionar o icone para adicionar o icone de foto
+  // para acessar a rota New la na navbar
+  static navigationOptions = {
+    headerRight: (
+      <TouchableOpacity style={{ marginRight: 20 }} onPress={() => {}}>
+        <Image source={camera} />
+      </TouchableOpacity>
+    )
+  };
+
   render() {
     return <View />;
   }
--- instagram-clone/instarocket/src/routes.js
+++ instagram-clone/instarocket/src/routes.js
@@ -1,12 +1,34 @@
-import { createAppContainer, createStackNavigator } from 'react-navigation';
+import React from "react";
+import { createAppContainer, createStackNavigator } from "react-navigation";
+import { Image, StyleSheet } from "react-native";
 
-import Feed from './pages/Feed';
-import New from './pages/New';
+import Feed from "./pages/Feed";
+import New from "./pages/New";
+
+import logo from "./assets/logo.png";
 
 // Necessário para definir as rotas da aplicação
+// createStackNavigator navigator cria uma navbar e seu modo de uso é a navegação em pilha
+// Existe também o createBottomTabNavigator que fica uma nav no lugar do footer
 export default createAppContainer(
-    createStackNavigator({
-        Feed,
-        New,
-    })
+  createStackNavigator(
+    {
+      Feed,
+      New
+    },
+    {
+      defaultNavigationOptions: {
+        headerTitle: (
+          <Image
+            style={{ width: 100, height: 30, flex: 1 }}
+            resizeMode="contain"
+            source={logo}
+          />
+        ),
+        headerBackTitle: null,
+        headerTintColor: "#000"
+      },
+      mode: "modal"
+    }
+  )
 );
