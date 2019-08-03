cca935e42173c5e78b09fb2f3d2fd8b9e012f9b3
Joberth Rogers <joberth.rogers18@gmail.com>
2019-08-02 23:21:06 -0400

    Added only backend dropbox-clone

---
+++ index.js
@@ -0,0 +1,35 @@
+const express = require('express');
+const app = express();
+const PORT = process.env.PORT || 3333;
+const routes = require('./src/routes');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const database = require('./src/config/database');
+const path =require('path');
+
+app.use(cors());
+
+const server = require('http').Server(app);
+const io = require('socket.io')(server);
+
+// Cria rooms separadas para quando fazer upload numa box, não apareça nas outras
+io.on('connection', socket => {
+    socket.on('connectRoom', box => {
+        socket.join(box);
+    })
+})
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(routes);
+app.use('/files', express.static(path.resolve(__dirname, 'tmp')));
+
+//passando uma informação global
+app.use((req, res, next) => {
+    req.io = io;
+    return next();
+});
+
+server.listen(PORT, () => {
+    console.log(`Server is on in port: ${PORT}`);
+});
--- react-native/caraCoroa/index.js
+++
@@ -1,5 +0,0 @@
-import { AppRegistry } from "react-native";
-import Routes from "./src";
-import { name as appName } from "./app.json";
-
-AppRegistry.registerComponent(appName, () => Routes);
