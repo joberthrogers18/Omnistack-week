e9979cf6f14e66ab667fa5fe8dc125455d837158
Joberth Rogers <joberth.rogers18@gmail.com>
2019-08-02 22:38:45 -0400

    Set up the realtime in dropbox-clone

--- dropbox-clone/backend/index.js
+++ dropbox-clone/backend/index.js
@@ -6,6 +6,15 @@ const bodyParser = require('body-parser');
 const cors = require('cors');
 const database = require('./src/config/database');
 const path =require('path');
+const server = require('http').Server(app);
+const io = require('socket.io')(server);
+
+// Cria rooms separadas para quando fazer upload numa box, não apareça nas outras
+io.on('connection', socket => {
+    socket.on('connectRoom', box => {
+        socket.join(box);
+    })
+})
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
@@ -13,6 +22,6 @@ app.use(cors());
 app.use(routes);
 app.use('/files', express.static(path.resolve(__dirname, 'tmp')));
 
-app.listen(PORT, () => {
+server.listen(PORT, () => {
     console.log(`Server is on in port: ${PORT}`);
 });
