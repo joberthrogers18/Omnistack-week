cd2e79d112ccada3eb515332827e1b3c5defcb52
Joberth Rogers <joberth.rogers18@gmail.com>
2019-08-08 00:37:22 -0400

    Added Main page and styling from  it

--- dropbox-clone/mobile/src/pages/Main/index.js
+++ dropbox-clone/mobile/src/pages/Main/index.js
@@ -1,12 +1,36 @@
 import React, { Component } from "react";
 
-import { View, Text } from "react-native";
+import { View, Text, Image, TextInput, TouchableOpacity } from "react-native";
+
+import styles from "./styles";
+import logo from "../../assets/logo.png";
 
 export default class Main extends Component {
+  state = {
+    box: ""
+  };
+
+  handleChange = event => {
+    this.setState({ ...this.state, box: event.target.data });
+  };
+
   render() {
     return (
-      <View>
-        <Text>Olá mundo!</Text>
+      <View style={styles.container}>
+        <Image style={styles.logo} source={logo} />
+        <TextInput
+          style={styles.input}
+          onChange={this.handleChange}
+          value={this.state.box}
+          placeholder="Crie um box"
+          placeholderTextColor="#999"
+          autoCapitalize="none"
+          autoCorrect={false}
+          underlineColorAndroid="transparent"
+        />
+        <TouchableOpacity onPress={() => {}} style={styles.button}>
+          <Text style={styles.buttonText}>Criar</Text>
+        </TouchableOpacity>
       </View>
     );
   }
