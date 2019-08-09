db50632b50a8f4a74065ac3c13fef35e5371c976
Joberth Rogers <joberth.rogers18@gmail.com>
2019-08-09 01:54:25 -0400

    finish the main in mobile dropbox

--- dropbox-clone/mobile/src/pages/Box/index.js
+++ dropbox-clone/mobile/src/pages/Box/index.js
@@ -6,6 +6,10 @@ import AsyncStorage from "@react-native-community/async-storage";
 import styles from "./styles";
 import api from "../../services/api";
 import Icon from "react-native-vector-icons/MaterialIcons";
+import ImagePicker from "react-native-image-picker";
+import RNRS from "react-native-fs";
+import FileViewer from "react-native-file-viewer";
+import socket from "socket.io-client";
 
 import { distanceInWords } from "date-fns";
 import pt from "date-fns/locale/pt";
@@ -17,13 +21,45 @@ export default class Box extends Component {
 
   async componentDidMount() {
     const idBox = await AsyncStorage.getItem("@RocketBox:box");
-    console.log(idBox);
+    this.subscribeToNewFiles();
+
     const response = await api.get(`boxes/${idBox}`);
     this.setState({ box: response.data });
   }
 
+  openFile = async file => {
+    try {
+      const filePath = `${RNRS.DocumentDirectoryPath}/${file.title}`;
+
+      console.log(filePath);
+
+      await RNRS.downloadFile({
+        fromUrl: file.url,
+        toFile: filePath
+      });
+
+      await FileViewer.open(filePath);
+    } catch (error) {
+      console.log("Arquivo não suportado");
+    }
+  };
+
+  subscribeToNewFiles = boxId => {
+    const io = socket("https://dropbox-clone-oministack.herokuapp.com/");
+
+    // Quando emiti a mensage; 'connectRoom' room ele vai colocar o novo
+    // host dentro do id da box
+    io.emit("connectRoom", boxId);
+
+    io.on("file", data => {
+      this.setState({
+        box: { ...this.state.box, files: [data, ...this.state.box.files] }
+      });
+    });
+  };
+
   renderItem = ({ item }) => (
-    <TouchableOpacity onPress={() => {}} style={styles.file}>
+    <TouchableOpacity onPress={() => this.openFile(item)} style={styles.file}>
       <View style={styles.fileInfo}>
         <Icon name="insert-drive-file" size={24} color="#A5CFFF" />
         <Text style={styles.fileTitle}>{item.title}</Text>
@@ -36,6 +72,29 @@ export default class Box extends Component {
       </Text>
     </TouchableOpacity>
   );
+
+  handleUpload = () => {
+    ImagePicker.launchImageLibrary({}, async upload => {
+      if (upload.error) {
+        console.log("Image picker error");
+      } else if (upload.didCancel) {
+        console.log("Canceled by user");
+      } else {
+        const data = new FormData();
+
+        const [prefix, suffix] = upload.fileName.split(".");
+        const ext = suffix.toLowerCase() === "heic" ? "jpg" : suffix;
+
+        data.append("file", {
+          uri: upload.uri,
+          type: upload.type,
+          name: `${prefix}.${ext}`
+        });
+
+        api.post(`boxes/${this.state.box._id}/files`, data);
+      }
+    });
+  };
 
   render() {
     return (
