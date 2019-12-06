1cb7792a3bb8f4413de072cf1515d9626d1f57a8
Joberth Rogers <joberth.rogers18@gmail.com>
2019-12-06 14:22:07 -0300

    Made modification in routes Added cors lib
    Signed-off-by: Joberth Rogers <joberth.rogers18@gmail.com>

--- aircnc/backend/src/server.js
+++ aircnc/backend/src/server.js
@@ -2,12 +2,14 @@ const express = require('express');
 const app = express();
 const routes = require('./routes');
 const dotenv = require('dotenv');
+const cors = require('cors');
 dotenv.config();
 
 require('./config/dbConfig');
 
 const PORT = process.env.PORT || 3333;
 
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 
