f321773c2a5d807cc7fac96c95a474654681c277
Joberth Rogers <joberth.rogers18@gmail.com>
2019-12-06 00:31:53 -0300

    Added new routes get and post Aircnc
    Signed-off-by: Joberth Rogers <joberth.rogers18@gmail.com>

--- aircnc/backend/src/controllers/SpotController.js
+++ aircnc/backend/src/controllers/SpotController.js
@@ -1,6 +1,16 @@
 const Spot = require('../models/Spot');
+const User = require('../models/User');
 
 module.exports = {
+    async index(req, res){
+        const { tech } = req.query;
+
+        // procura tech dentro de um array de techs
+        const spots = await Spot.find({ techs: tech });
+
+        return res.json(spots);
+    },
+
     async store(req, res){
         const { company, price, techs } = req.body;
         const thumbnail = req.file.filename;
@@ -11,12 +21,14 @@ module.exports = {
             tech.trim();
         });
 
-        console.log(arrayTechs);
-
         const { user_id } = req.headers;
 
-        console.log(thumbnail);
-        
+        const user = await User.findById(user_id);
+
+        if(!user){
+            return res.status(400).json({ error: "User does not exists!!"});
+        }
+ 
         const spot = await Spot.create({
             thumbnail, 
             company,
--- aircnc/backend/src/routes.js
+++ aircnc/backend/src/routes.js
@@ -6,11 +6,19 @@ const uploadConfig = require('./config/uploadConfig');
 
 const SessionController = require('./controllers/SessionControllers');
 const SpotController = require('./controllers/SpotController');
+const DashboardController = require('./controllers/DashboardController');
+const BookingController = require('./controllers/BookingController');
 
 const upload = multer(uploadConfig);
 
 routes.post('/users', SessionController.store);
+
+routes.get('/spots', SpotController.index);
 routes.post('/spots', upload.single('thumbnail') ,SpotController.store);
+
+routes.get('/dashboard', DashboardController.show);
+
+routes.post('/spot/:spot_id/bookings', BookingController.store);
 
 
 module.exports = routes;
