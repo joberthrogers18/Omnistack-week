2a0ee8f5b2f98d19a59818bd0088ca367d7028d6
Joberth Rogers <joberth.rogers18@gmail.com>
2019-12-12 10:44:18 -0400

    Treated the messages come to socket from backend
    List the booking request to owner spots, stled the request
    
    Signed-off-by: Jobert Rogers <joberth.rogers18@gmail.com>

--- aircnc/frontend/src/pages/Dashboard/index.js
+++ aircnc/frontend/src/pages/Dashboard/index.js
@@ -1,11 +1,26 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState, useMemo } from 'react';
 import api from '../../services/configApi';
 import './styles.css';
 import { Link } from 'react-router-dom';
+import socketio from 'socket.io-client';
 
 function Dashboard(){
 
     const [spots, setSpots] = useState([]);
+    const [requests, setRequests] = useState([]);
+    
+    const user_id = localStorage.getItem('user');
+    const socket = useMemo(() => socketio('http://localhost:3333', {
+        query: { user_id }
+    }), [user_id]);
+
+    useEffect(() => {
+
+        socket.on('booking_request', data => {
+            setRequests([...requests, data]);
+        });
+
+    }, [requests, socket]);
 
     // se deixar vazio o parametro de lista, ele executa apenas uma vez
     useEffect(() => {
@@ -26,6 +41,18 @@ function Dashboard(){
 
     return (
         <>
+            <ul className="notifications" >
+                {requests.map(request => (
+                    <li key={request._id}>
+                        <p>
+                            <strong>{request.user.email}</strong> está solicitando uma reserva em  <strong>{request.spot.company}</strong> para a data: <strong>{request.date}</strong>
+                        </p>
+                        <button className="accept">ACEITAR</button>
+                        <button className="refuse">REJEITAR</button>
+                    </li>
+                ))}
+            </ul>
+
             <ul className="spot-list">
                 {spots.map(spot =>(
                     <li key={spot._id}>
