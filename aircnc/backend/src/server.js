6d68752812933618fb33428fd7501a77e65a9bfe
Joberth Rogers <joberth.rogers18@gmail.com>
2019-12-12 10:42:06 -0400

    Setup the socket backend and mesagem emit to client
    Signed-off-by: Joberth Rogers <joberth.rogers18@gmail.com>

--- aircnc/backend/src/server.js
+++ aircnc/backend/src/server.js
@@ -1,14 +1,37 @@
 const express = require('express');
-const app = express();
 const routes = require('./routes');
 const dotenv = require('dotenv');
 const cors = require('cors');
 const path = require('path');
+
+const socketio = require('socket.io');
+const http = require('http');
+
+const app = express();
+const server = http.Server(app);
+const io = socketio(server);
+
 dotenv.config();
 
 require('./config/dbConfig');
 
 const PORT = process.env.PORT || 3333;
+
+const connectedUsers = {};
+
+io.on('connection', socket => {
+    const { user_id } = socket.handshake.query;
+
+    // salva o id de usúario com o id do socket de quando ele conecta
+    connectedUsers[user_id] = socket.id;
+});
+
+app.use((req,res, next) => {
+    req.io = io;
+    req.connectedUsers = connectedUsers;
+
+    return next();
+});
 
 app.use(cors());
 app.use(express.json());
@@ -20,6 +43,6 @@ app.use('/files', express.static(path.resolve( __dirname, '..', 'uploads')));
 
 app.use(routes);
 
-app.listen(PORT, () => {
+server.listen(PORT, () => {
     console.log(`The server is running in port ${PORT}`);
 });
