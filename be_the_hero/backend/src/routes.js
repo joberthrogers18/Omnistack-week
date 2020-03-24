3f49887facf108b524cc86ea569f3be75dfd1cb9
Joberth Rogers <joberth.rogers18@gmail.com>
2020-03-24 17:54:06 -0400

    Added route to get incidents from ong and for fulffill a login
    Signed-off-by: Joberth Rogers <joberth.rogers18@gmail.com>

--- be_the_hero/backend/src/routes.js
+++ be_the_hero/backend/src/routes.js
@@ -3,6 +3,8 @@ const routes = express.Router();
 
 const OngsControllers = require('./controllers/OngsControllers');
 const IncidentsControllers = require('./controllers/IncidentController');
+const ProfileController = require('./controllers/ProfileController');
+const SessionController = require('./controllers/SessionController');
 
 routes.get('/', (req, res) => {
   return res.json({server: 'running successfully'});
@@ -16,5 +18,11 @@ routes.get('/ongs', OngsControllers.show);
 routes.post('/incidents', IncidentsControllers.create);
 routes.get('/incidents', IncidentsControllers.show);
 routes.delete('/incidents/:id', IncidentsControllers.delete);
+
+// get incidents from a unique ong
+routes.get('/profile', ProfileController.index);
+
+// Session
+routes.post('/login', SessionController.create);
 
 module.exports = routes;
