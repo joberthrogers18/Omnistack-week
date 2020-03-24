34b22a522db1b09306e3d4d74e356fc77699e892
Joberth Rogers <joberth.rogers18@gmail.com>
2020-03-24 15:16:27 -0400

    Created the main struct of databse with knex query build and sqlite
    Signed-off-by: Joberth Rogers <joberth.rogers18@gmail.com>

--- be_the_hero/backend/src/index.js
+++ be_the_hero/backend/src/index.js
@@ -1,12 +1,11 @@
 const express = require('express');
 const app = express();
+const routes = require('./routes'); 
+
 const PORT = process.env.PORT || 3333;
 
 app.use(express.json())
-
-app.get('/', (req, res) => {
-  res.json({hello: 'world'});
-});
+app.use(routes);
 
 app.listen(PORT, () => {
   console.log(`Server is running in ther port ${PORT}`);
