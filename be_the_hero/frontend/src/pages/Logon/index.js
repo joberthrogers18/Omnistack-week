001d213d5739c19b185de7f6b89066865f9ef3e0
Joberth Rogers <joberth.rogers18@gmail.com>
2020-03-25 18:44:12 -0400

    Finished logon page style
    Signed-off-by: Joberth Rogers <joberth.rogers18@gmail.com>

--- be_the_hero/frontend/src/pages/Logon/index.js
+++ be_the_hero/frontend/src/pages/Logon/index.js
@@ -1,9 +1,29 @@
 import React from 'react';
+import { FiLogIn } from 'react-icons/fi';
 
 import './styles.css';
+import herosImage from '../../assets/heroes.png';
+import logoHeroes from '../../assets/logo.svg'
 
 export default function Logon() {
   return (
-    <h1>Logon</h1>
+    <div className="logon-container">
+      <section className="form">
+        <img src={logoHeroes} alt="Be the Hero"/>
+        
+        <form>
+          <h1>Faça seu logon</h1>
+
+          <input placeholder="Sua id"/>
+          <button className="button" type="submit">Entrar</button>
+
+          <a href="/register">
+            <FiLogIn size={16} color="#E02041"/>
+            Não tenho cadastro
+          </a>
+        </form>
+      </section>
+      <img src={herosImage} alt="Heroes"/>
+    </div>
   )
 }
