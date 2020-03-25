d4bd190a7ac81a20e3ff025c4a3f056aa6e942e3
Joberth Rogers <joberth.rogers18@gmail.com>
2020-03-25 18:16:37 -0400

    Stylling the main pages and add routes
    Signed-off-by: Joberth Rogers <joberth.rogers18@gmail.com>

--- be_the_hero/frontend/src/App.js
+++ be_the_hero/frontend/src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 
-import Header from './Header';
+import './global.css';
+
+import Router from './routes';
 
 function App() {
   return (
-    <Header />
+    <Router />
   );
 }
 
