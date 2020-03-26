a551eb290b1b8021b8d31594d038339f6357eeda
Joberth Rogers <joberth.rogers18@gmail.com>
2020-03-25 23:40:53 -0400

    Finished Profile page connection
    Signed-off-by: Joberth Rogers <joberth.rogers18@gmail.com>

--- be_the_hero/frontend/src/pages/Logon/index.js
+++ be_the_hero/frontend/src/pages/Logon/index.js
@@ -1,21 +1,45 @@
-import React from 'react';
-import { Link } from 'react-router-dom'
+import React, { useState } from 'react';
+import { Link, useHistory } from 'react-router-dom'
 import { FiLogIn } from 'react-icons/fi';
+
+import api from '../../services/api';
 
 import './styles.css';
 import herosImage from '../../assets/heroes.png';
 import logoHeroes from '../../assets/logo.svg'
 
 export default function Logon() {
+  const [id, setId] = useState('');
+
+  const history = useHistory();
+
+  async function handleLogin(event) {
+    event.preventDefault();
+
+    try {
+      const response = await api.post('login', { id });
+
+      localStorage.setItem('ongId', id);
+      localStorage.setItem('ongName', response.data.name);
+      history.push('/profile');
+    } catch(e) {
+      alert('Falha no login, tente novamente');
+    }
+  }
+
   return (
     <div className="logon-container">
       <section className="form">
         <img src={logoHeroes} alt="Be the Hero"/>
         
-        <form>
+        <form onSubmit={handleLogin}>
           <h1>Faça seu logon</h1>
 
-          <input placeholder="Sua id"/>
+          <input
+            value={id}
+            onChange={e => setId(e.target.value)} 
+            placeholder="Sua id"
+          />
           <button className="button" type="submit">Entrar</button>
           <Link className="backlink" to="/register">
               <FiLogIn size={16} color="#E02041"/>
