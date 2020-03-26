c6239144dd1bafead8c4fe6265be772e71073780
Joberth Rogers <joberth.rogers18@gmail.com>
2020-03-25 22:26:17 -0400

    Added cors to backend
    Signed-off-by: Joberth Rogers <joberth.rogers18@gmail.com>

--- be_the_hero/backend/src/index.js
+++ be_the_hero/backend/src/index.js
@@ -1,8 +1,11 @@
 const express = require('express');
 const app = express();
 const routes = require('./routes'); 
+const cors = require('cors');
 
 const PORT = process.env.PORT || 3333;
+
+app.use(cors());
 
 app.use(express.json())
 app.use(routes);
