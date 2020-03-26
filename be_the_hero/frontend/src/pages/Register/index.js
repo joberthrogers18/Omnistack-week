b2e433faf5ff632cae6f271e0056e964837401d0
Joberth Rogers <joberth.rogers18@gmail.com>
2020-03-25 20:55:46 -0400

    Added component profile
    Signed-off-by: Joberth Rogers <joberth.rogers18@gmail.com>

--- be_the_hero/frontend/src/pages/Register/index.js
+++ be_the_hero/frontend/src/pages/Register/index.js
@@ -14,7 +14,7 @@ export default function Register() {
 
           <h1>Cadastro</h1>
           <p>Faça seu cadastro, entre na plataforma e ajude pessoas a encontrarem os casos da sua ONG.</p>
-          <Link className="backlink" to="/register">
+          <Link className="backlink" to="/">
               <FiArrowLeft size={16} color="#E02041"/>
               Não tenho cadastro
           </Link>
