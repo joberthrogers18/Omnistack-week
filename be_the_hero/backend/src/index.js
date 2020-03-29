bcfd79822ca41a8d3ee271befe155c1c7182ea55
Joberth Rogers <joberth.rogers18@gmail.com>
2020-03-29 16:41:53 -0400

    Added validation with celebrate lib in backend
    Signed-off-by: Joberth Rogers <joberth.rogers18@gmail.com>

--- be_the_hero/backend/src/index.js
+++ be_the_hero/backend/src/index.js
@@ -1,7 +1,8 @@
 const express = require('express');
 const app = express();
-const routes = require('./routes'); 
+const routes = require('./routes');
 const cors = require('cors');
+const { errors } = require('celebrate');
 
 const PORT = process.env.PORT || 3333;
 
@@ -9,6 +10,9 @@ app.use(cors());
 
 app.use(express.json())
 app.use(routes);
+
+// retrun the error in validation from celebrate formated
+app.use(errors());
 
 app.listen(PORT, () => {
   console.log(`Server is running in ther port ${PORT}`);
--- be_the_hero/backend/src/routes.js
+++ be_the_hero/backend/src/routes.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const { celebrate, Segments, Joi } = require('celebrate');
 const routes = express.Router();
 
 const OngsControllers = require('./controllers/OngsControllers');
@@ -7,20 +8,41 @@ const ProfileController = require('./controllers/ProfileController');
 const SessionController = require('./controllers/SessionController');
 
 routes.get('/', (req, res) => {
-  return res.json({server: 'running successfully'});
+  return res.json({ server: 'running successfully' });
 });
 
 // Routes to ongs
-routes.post('/ongs', OngsControllers.create);
+// validation using celebrate
+routes.post('/ongs', celebrate({
+  [Segments.BODY]: Joi.object().keys({
+    name: Joi.string().required(),
+    email: Joi.string().required().email(),
+    whatsapp: Joi.number().required().min(10).max(11),
+    city: Joi.string().required(),
+    uf: Joi.string().required().length(2)
+  })
+}), OngsControllers.create);
 routes.get('/ongs', OngsControllers.show);
 
 //Routes to incidents
 routes.post('/incidents', IncidentsControllers.create);
-routes.get('/incidents', IncidentsControllers.show);
-routes.delete('/incidents/:id', IncidentsControllers.delete);
+routes.get('/incidents', celebrate({
+  [Segments.QUERY]: Joi.object().keys({
+    page: Joi.number()
+  })
+}), IncidentsControllers.show);
+routes.delete('/incidents/:id', celebrate({
+  [Segments.PARAMS]: Joi.object().keys({
+    id: Joi.number().required()
+  })
+}), IncidentsControllers.delete);
 
 // get incidents from a unique ong
-routes.get('/profile', ProfileController.index);
+routes.get('/profile', celebrate({
+  [Segments.HEADERS]: Joi.object({
+    authorization: Joi.string().required()
+  }).unknown()
+}), ProfileController.index);
 
 // Session
 routes.post('/login', SessionController.create);
